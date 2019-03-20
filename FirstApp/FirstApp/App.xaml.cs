ff8aa1613879f1e0cb9395ae71e67d1f46d5c498
milano95a@gmail.com <abdu1995>
2019-03-20 19:22:34 +0500

    Add project files.

---
+++ FirstApp/FirstApp/App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace FirstApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var layout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children =
+                    {
+                        new Label
+                        {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+            };
+
+            MainPage = new ContentPage
+            {
+                Content = layout
+            };
+
+            Button button = new Button
+            {
+                Text = "Click Me"
+            };
+
+            button.Clicked += async (s, e) =>
+            {
+                await MainPage.DisplayAlert("Alert", "You clicked me", "OK");
+            };
+
+            layout.Children.Add(button);
+        }
+        //public App()
+        //{
+        //    InitializeComponent();
+
+        //    MainPage = new MainPage();
+        //}
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
